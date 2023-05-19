5b1a30be228689792d8b9bbb4594f014a0fcb272
1 <1@LAPTOP-ILGMJ5ES>
2023-05-19 11:24:41 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/Forms/ChangesWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using WpfApp2.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для ChangesWindow.xaml
+    /// </summary>
+    public partial class ChangesWindow : Window
+    {
+        public Product Product { get; set; }
+        public ChangesWindow(Product product)
+        {
+            InitializeComponent();
+            Product = product;
+            DataContext = Product;
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            Close();
+            mainWindow.ShowDialog();
+        }
+
+        private void edit_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+
+        }
+    }
+}
